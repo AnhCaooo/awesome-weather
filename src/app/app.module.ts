b2030fa41ab82bb2ff3bbb9017ee4fb81930e09b
anhcao <anhcao4922@gmail.com>
2022-11-08 19:21:59 +0200

    Implemented the basic outlook of the application
    Signed-off-by: anhcao <anhcao4922@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
+// Components
 import { AppComponent } from './app.component';
+import { MainSiteComponent } from './components/main-site/main-site.component';
+import { AboutSiteComponent } from './components/about-site/about-site.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainSiteComponent,
+    AboutSiteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
